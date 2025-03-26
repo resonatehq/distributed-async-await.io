ea2350845fb05cb1b7aed7c68028aff464fc162f
Cully Wakelin <cully.wakelin@resonatehq.io>
2025-03-26 13:57:42 -0600

    diagrams and content updates

---
+++ src/components/DAACallOut/DAACallOut.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const DAACallOut = ({ children }) => (
+  <p
+    style={{
+      borderColor: "#3cc9a7",
+      borderWidth: "0 0 0 4px",
+      paddingLeft: "8px",
+      borderStyle: "solid",
+    }}
+  >
+    {children}
+  </p>
+);
